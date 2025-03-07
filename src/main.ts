809df9e5c51b93d470395c0e679bd305c219f19e
LiusDev <quydx.work@gmail.com>
2025-03-07 11:23:16 +0700

    chore: swagger UI

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config'
 import { ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { initializeFfmpeg } from './utils/ffmpeg.helper'
+import * as tf from '@tensorflow/tfjs'
 
 async function bootstrap() {
     // Initialize ffmpeg before starting the application
@@ -22,12 +23,15 @@ async function bootstrap() {
     app.useLogger(logger)
     const configService = app.get(ConfigService)
     const config = new DocumentBuilder()
-        .setTitle('MindGPT AI Hub')
-        .setDescription('MindGPT AI Hub API description')
+        .setTitle('Quickcap AI')
+        .setDescription('Quickcap AI API Documentation')
         .setVersion('1.0')
         .build()
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('api', app, document)
+    if (configService.get('NODE_ENV') === 'production') {
+        tf.enableProdMode()
+    }
     logger.log(`Running on port ${configService.get('PORT')}`)
     await app.listen(configService.get('PORT') || 3000)
 }
