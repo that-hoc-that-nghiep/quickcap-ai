bf36bb8245b2318d123f8a0f4a53bcf420b9a183
LiusDev <quydx.work@gmail.com>
2025-03-12 17:46:31 +0700

    fix: remove unused module

--- src/s3/s3.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { S3Service } from './s3.service'
-
-@Module({
-    exports: [S3Service],
-    providers: [S3Service]
-})
-export class S3Module {}
--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common'
 import { VideoService } from './video.service'
 import { VideoController } from './video.controller'
 import { AiModule } from 'src/ai/ai.module'
-import { S3Module } from 'src/s3/s3.module'
 import { FfmpegModule } from 'src/ffmpeg/ffmpeg.module'
 import { RabbitmqModule } from 'src/rabbitmq/rabbitmq.module'
 import { WorkerThreadsModule } from '../worker-threads/worker-threads.module'
 
 @Module({
-    imports: [AiModule, S3Module, FfmpegModule, RabbitmqModule, WorkerThreadsModule],
+    imports: [AiModule, FfmpegModule, RabbitmqModule, WorkerThreadsModule],
     controllers: [VideoController],
     providers: [VideoService]
 })
