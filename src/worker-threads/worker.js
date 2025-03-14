cefe9ce46552cf3bb8cd41da5499789ff0245ced
Dao Xuan Quy <quydx.work@gmail.com>
2025-03-14 09:37:51 +0700

    Update worker.js

--- src/worker-threads/worker.js
+++ src/worker-threads/worker.js
@@ -114,7 +114,7 @@ async function processFramesForNSFW(frameFiles) {
     // Load NSFW model if not already loaded
     if (!nsfwModel) {
         console.log('Loading NSFW model...')
-        nsfwModel = await nsfwjs.load()
+        nsfwModel = await nsfwjs.load("InceptionV3")
         console.log('NSFW model loaded successfully')
     }
 
